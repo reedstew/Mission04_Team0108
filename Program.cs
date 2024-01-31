232e8bba495371d2aac3382efafa39fc540a2d10
stewyman <stewyman@REEDSTEWARTE3F6>
2024-01-30 20:51:43 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Authors: Reed Stewart, Matt Cooper. Jake Millet, Caleb Reese
+// Description: The game of Tic-Tac-Toe in C#.
+
+// The "Driver" Class will be written below:
+/* The “Driver” class (the Program.cs class with the main method where the program begins)
+will:
+• Welcome the user to the game
+• Create a game board array to store the players’ choices
+• Ask each player in turn for their choice and update the game board array
+• Print the board by calling the method in the supporting class
+• Check for a winner by calling the method in the supporting class, and notify the players
+when a win has occurred and which player won the game */
+
+public class Driver
+    {
+        
+
+    }
---
+++ Support.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+namespace Mission04_Team0108
+{
+    public class Support
+    {
+        // The supporting class will be written below:
+        /*The supporting class (with whichever name you choose) will:
+            • Receive the “board” array from the driver class
+            • Contain a method that prints the board based on the information passed to it
+            • Contain a method that receives the game board array as input and returns if there is a
+                winner and who it was*/
+
+    }
+}
