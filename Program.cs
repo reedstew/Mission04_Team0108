3252e0dd39850f14b9fc06e7921bb669700eb592
jacobmillett <jakemillett1@gmail.com>
2024-02-01 11:12:33 -0700

    All Working

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ will:
 • Check for a winner by calling the method in the supporting class, and notify the players
 when a win has occurred and which player won the game */
 
+using Mission04_Team0108;
 using System.Numerics;
 using System.Threading.Tasks;
 
@@ -20,7 +21,7 @@ public class Driver
     public static void Main()
     {
         bool GameWon = false;
-
+        int turnTracker = 0;
         // Welcome the user to the game
         Console.Write("Welcome to the game of Tic-Tac-Toe");
         Thread.Sleep(400);
@@ -30,14 +31,13 @@ public class Driver
         Thread.Sleep(400);
         Console.Write('.');
         Console.WriteLine("");
-
+        Support s = new Support();
         //Create a game board array to store the players’ choices
 
         //Define 3x3
         char[,] board = new char[3, 3];
         // Player arrays
-        char[,] playerOne = new char[3, 3];
-        char[,] playerTwo = new char[3, 3];
+        bool WinnerFlag = false;
 
         // Populate
         for (int i = 0; i < 3; i++)
@@ -51,68 +51,59 @@ public class Driver
 
         do
         {
-
+            string row = "";
+            string column = "";
 
             // Ask each player in turn for their choice and update the game board array
 
             // First players turn
-            Console.WriteLine("Player One: Choose a row. Type 'TOP', 'MIDDLE', or 'BOTTOM'.");
-            string pOneRow = Console.ReadLine();
-            ValidateRow(pOneRow);
-            pOneRow = pOneRow.ToUpper();
-
-            Console.WriteLine("Player One: Choose a column. Type 'LEFT', 'CENTER', or 'RIGHT'.");
-            string pOneColumn = Console.ReadLine();
-            ValidateColumn(pOneColumn);
-            pOneColumn = pOneColumn.ToUpper();
-
-            if (pOneRow.Equals("TOP"))
+            if (turnTracker == 0)
             {
-                playerOne[0, 1] = "X";
+                Console.WriteLine("Player One: Choose a row. Type 'TOP', 'MIDDLE', or 'BOTTOM'.");
+                row = Console.ReadLine();
+                row = ValidateRow(row);
+                Console.WriteLine("Player One: Choose a column. Type 'LEFT', 'CENTER', or 'RIGHT'.");
+                column = Console.ReadLine();
+                column = ValidateColumn(column);
             }
-            else if (pOneRow.Equals("MIDDLE"))
+            else if (turnTracker == 1)
             {
+                Console.WriteLine("Player Two: Choose a row. Type 'TOP', 'MIDDLE', or 'BOTTOM'.");
+                row = Console.ReadLine();
+                row = ValidateRow(row);
 
-            }
-            else
-            {
+                Console.WriteLine("Player Two: Choose a column. Type 'LEFT', 'CENTER', or 'RIGHT'.");
+                column = Console.ReadLine();
+                column = ValidateColumn(column);
 
             }
 
-            // Second players turn
-            Console.WriteLine("Player Two: Choose a row. Type 'TOP', 'MIDDLE', or 'BOTTOM'.");
-            string pTwoRow = Console.ReadLine();
-            ValidateRow(pTwoRow);
-            pTwoRow = pTwoRow.ToUpper();
 
-            Console.WriteLine("Player Two: Choose a column. Type 'LEFT', 'CENTER', or 'RIGHT'.");
-            string pTwoColumn = Console.ReadLine();
-            ValidateColumn(pTwoColumn);
-            pTwoColumn = pTwoColumn.ToUpper();
 
-            if (pTwoRow.Equals("TOP"))
+            turnTracker++;
+            if (turnTracker == 2)
             {
-
-            }
-            else if (pTwoRow.Equals("MIDDLE"))
-            {
-
+                turnTracker = 0;
+                board = s.UpdateBoard(board, row, column, turnTracker);
             }
             else
             {
-
+                board = s.UpdateBoard(board, row, column, turnTracker);
             }
 
+            GameWon = s.Winner(board, WinnerFlag);
         } while (!GameWon);
     }
-    public static void ValidateRow(string choice)
+    public static string ValidateRow(string choice)
     {
+        string validRow = "";
         bool valid = false;
         do
         {
             choice = choice.ToUpper();
             if ((choice == "TOP" || choice == "MIDDLE" || choice == "BOTTOM"))
             {
+                validRow = choice;
                 valid = true;
             }
             else
@@ -120,17 +111,20 @@ public class Driver
                 Console.WriteLine("Please choose a valid row. Type 'TOP', 'MIDDLE', or 'BOTTOM'.");
                 choice = Console.ReadLine();
             }
-        } while (valid);
+        } while (!valid);
+        return validRow;
     }
 
-    public static void ValidateColumn(string choice)
+    public static string ValidateColumn(string choice)
     {
+        string validColumn = "";
         bool valid = false;
         do
         {
             choice = choice.ToUpper();
             if ((choice == "LEFT" || choice == "CENTER" || choice == "RIGHT"))
             {
+                validColumn = choice;
                 valid = true;
             }
             else
@@ -138,8 +132,10 @@ public class Driver
                 Console.WriteLine("Please choose a valid column. Type 'LEFT', 'CENTER', or 'RIGHT'.");
                 choice = Console.ReadLine();
             }
-        } while (valid);
+        } while (!valid);
+        return validColumn;
     }
+
 
     public static bool CheckForWinner(char[,] board)
     {
--- Support.cs
+++ Support.cs
@@ -12,32 +12,36 @@ namespace Mission04_Team0108
 {
     public class Support
     {
+
         // The supporting class will be written below:
         /*The supporting class (with whichever name you choose) will:
             • Receive the “board” array from the driver class
             • Contain a method that prints the board based on the information passed to it
             • Contain a method that receives the game board array as input and returns if there is a
                 winner and who it was*/
-        int[,] board = new int[3, 3];
-        int rowIndex = 0;
-        int columnIndex = 0;
-        int turnTracker = 0;
+
+
+
         bool winnerFlag = false;
         bool[] winningSituations = new bool[8];
         string winningTeam = "";
-        
-        public Array UpdateBoard(board, row, column)
-        {   
+
+        public char[,] UpdateBoard(char[,] board, string row, string column, int turnTracker)
+        {
+            int rowIndex = 0;
+            int columnIndex = 0;
+            row = row.ToUpper();
+            column = column.ToUpper();
             //These if statements are changing the top, middle, bottom, left, middle, right into the numbers in the array
-            if (row[0].lower() == 't')
+            if (row == "TOP")
             {
                 rowIndex = 0;
             }
-            else if (rowIndex[0].lower() =='m')
+            else if (row == "MIDDLE")
             {
                 rowIndex = 1;
             }
-            else if (rowIndex[0].lower() == 'b')
+            else if (row == "BOTTOM")
             {
                 rowIndex = 2;
             }
@@ -46,78 +50,83 @@ namespace Mission04_Team0108
                 Console.WriteLine("Input for row is not valid");
             }
 
-            if (column[0].lower() == 'l')
+            if (column == "LEFT")
             {
                 columnIndex = 0;
             }
-            else if (column[0].lower(0) == 'm')
+            else if (column == "CENTER")
             {
                 columnIndex = 1;
             }
-            else if (column[0].lower() == 'r')
+            else if (column == "RIGHT")
             {
                 columnIndex = 2;
             }
             else
             {
                 Console.WriteLine("Input for column is not valid");
-
+            }
             //This is deciding whether it gets changed with an O or an X using the turnTracker variable
-            if (turnTracker == 0)
+            if (turnTracker == 1)
             {
                 board[rowIndex, columnIndex] = 'X';
             }
-            else if (turnTracker == 1)
+            else if (turnTracker == 0)
+            {
                 board[rowIndex, columnIndex] = 'O';
+            }
 
-            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine(); // Move to the next line for the next row
             }
 
             //This is what keeps the turntracker where it needs to be for changing turns
-            turnTracker++;
-            if (turnTracker == 2)
-            {
-                turnTracker = 0;
-            }
+
+
 
 
             return (board);
 
         }
 
-        public bool Winner(board)
+        public bool Winner(char[,] board, bool winnerFlag)
         {
             //This is going through all of the winning situations and changing a boolean array I created to true if it is true.
             //This also determines the winning team
-            if (board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2])
+            if ((board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2]) && board[0, 0] != '#')
             {
                 winningSituations[0] = true;
                 winningTeam = board[0, 2].ToString();
             }
             else winningSituations[0] = false;
 
-            if (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2])
+            if ((board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2]) && board[1, 0] != '#')
             {
                 winningSituations[1] = true;
                 winningTeam = board[1, 2].ToString();
             }
             else winningSituations[1] = false;
 
-            if (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])
+            if ((board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2]) && board[2, 0] != '#')
             {
                 winningSituations[2] = true;
                 winningTeam = board[2, 2].ToString();
             }
             else winningSituations[2] = false;
 
-            if (board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0])
+            if ((board[0, 0] == board[1, 0] && board[1, 0] == board[2, 0]) && board[0, 0] != '#')
             {
                 winningSituations[3] = true;
                 winningTeam = board[2, 0].ToString();
             }
             else winningSituations[3] = false;
 
-            if (board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1])
+            if ((board[0, 1] == board[1, 1] && board[1, 1] == board[2, 1]) && board[0, 1] != '#')
             {
                 winningSituations[4] = true;
                 winningTeam = board[2, 1].ToString();
@@ -127,7 +136,7 @@ namespace Mission04_Team0108
                 winningSituations[4] = false;
             }
 
-            if (board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2])
+            if ((board[0, 2] == board[1, 2] && board[1, 2] == board[2, 2]) && board[0, 2] != '#')
             {
                 winningSituations[5] = true;
                 winningTeam = board[2, 2].ToString();
@@ -137,31 +146,31 @@ namespace Mission04_Team0108
                 winningSituations[5] = false;
             }
 
-            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            if ((board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) && board[0, 0] != '#')
             {
                 winningSituations[6] = true;
                 winningTeam = board[2, 2].ToString();
             }
-            else 
+            else
             {
-                winningSituations[6] = false; 
+                winningSituations[6] = false;
             }
 
-            if (board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2])
+            if ((board[2, 0] == board[1, 1] && board[1, 1] == board[0, 2]) && board[2, 0] != '#')
             {
                 winningSituations[7] = true;
                 winningTeam = board[0, 2].ToString();
             }
-            else 
+            else
             {
-                winningSituations[7] = false; 
+                winningSituations[7] = false;
             }
             // This goes through all of the winning situations array and if any of them are true it changes the winner flag to true.
             for (int i = 0; i < winningSituations.Length; i++)
             {
                 if (winningSituations[i] == true)
                 {
-
+                    Console.WriteLine($"{winningTeam}'s win!!!");
                     winnerFlag = true;
                     break; // Exit the loop since we found a true value
                 }
